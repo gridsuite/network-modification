b2a53a2db5d3f0789db2f3710a9f3cf57f6e935e
Joris Mancini <joris.mancini_externe@rte-france.com>
2025-05-23 16:52:08 +0200

    test: add tests
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

--- src/test/java/org/gridsuite/modification/modifications/BalancesAdjustmentModificationTest.java
+++ src/test/java/org/gridsuite/modification/modifications/BalancesAdjustmentModificationTest.java
@@ -36,9 +36,30 @@ class BalancesAdjustmentModificationTest extends AbstractNetworkModificationTest
                 BalancesAdjustmentAreaInfos.builder()
                     .name("FR")
                     .countries(List.of(Country.FR))
-                    .netPosition(80d)
+                    .netPosition(-45d)
                     .shiftType(ShiftType.PROPORTIONAL)
                     .shiftEquipmentType(ShiftEquipmentType.GENERATOR)
+                    .build(),
+                BalancesAdjustmentAreaInfos.builder()
+                    .name("NE")
+                    .countries(List.of(Country.NE))
+                    .netPosition(-54d)
+                    .shiftType(ShiftType.BALANCED)
+                    .shiftEquipmentType(ShiftEquipmentType.GENERATOR)
+                    .build(),
+                BalancesAdjustmentAreaInfos.builder()
+                    .name("GE")
+                    .countries(List.of(Country.GE))
+                    .netPosition(0d)
+                    .shiftType(ShiftType.PROPORTIONAL)
+                    .shiftEquipmentType(ShiftEquipmentType.LOAD)
+                    .build(),
+                BalancesAdjustmentAreaInfos.builder()
+                    .name("AU")
+                    .countries(List.of(Country.AU))
+                    .netPosition(100d)
+                    .shiftType(ShiftType.BALANCED)
+                    .shiftEquipmentType(ShiftEquipmentType.LOAD)
                     .build()
             ))
             .build();
@@ -46,13 +67,22 @@ class BalancesAdjustmentModificationTest extends AbstractNetworkModificationTest
 
     @Override
     protected void assertAfterNetworkModificationApplication() {
-        assertEquals(-82.4d, getNetwork().getGenerator("GH1").getTerminal().getP(), 0.1);
-        assertEquals(-84.1d, getNetwork().getGenerator("GH2").getTerminal().getP(), 0.1);
-        assertEquals(-149.8d, getNetwork().getGenerator("GH3").getTerminal().getP(), 0.1);
+        assertEquals(-58.4d, getNetwork().getGenerator("GH1").getTerminal().getP(), 0.1);
+        assertEquals(-36d, getNetwork().getGenerator("GH2").getTerminal().getP(), 0.1);
+        assertEquals(-101.8d, getNetwork().getGenerator("GH3").getTerminal().getP(), 0.1);
+        assertEquals(-100d, getNetwork().getGenerator("GTH1").getTerminal().getP(), 0.1);
+        assertEquals(-146.9d, getNetwork().getGenerator("GTH2").getTerminal().getP(), 0.1);
+
+        assertEquals(80.2d, getNetwork().getLoad("LD1").getTerminal().getP(), 0.1);
+        assertEquals(60.2d, getNetwork().getLoad("LD2").getTerminal().getP(), 0.1);
+        assertEquals(60.2d, getNetwork().getLoad("LD3").getTerminal().getP(), 0.1);
+        assertEquals(40.1d, getNetwork().getLoad("LD4").getTerminal().getP(), 0.1);
+        assertEquals(200.5d, getNetwork().getLoad("LD5").getTerminal().getP(), 0.1);
+        assertEquals(0d, getNetwork().getLoad("LD6").getTerminal().getP(), 0.1);
     }
 
     @Override
     protected void checkModification() {
-
+        // No checks implemented for this modification
     }
 }
