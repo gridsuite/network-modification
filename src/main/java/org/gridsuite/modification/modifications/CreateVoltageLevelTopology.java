63a87e1e685889e735ce9863565950868f2c9ad1
Etienne LESOT <etienne.lesot@rte-france.com>
2025-06-11 08:42:07 +0200

    first try
    Signed-off-by: Etienne LESOT <etienne.lesot@rte-france.com>

--- src/main/java/org/gridsuite/modification/modifications/CreateVoltageLevelTopology.java
+++ src/main/java/org/gridsuite/modification/modifications/CreateVoltageLevelTopology.java
@@ -6,14 +6,20 @@
  */
 package org.gridsuite.modification.modifications;
 
+import com.powsybl.commons.PowsyblException;
 import com.powsybl.commons.report.ReportNode;
+import com.powsybl.iidm.network.BusbarSection;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.SwitchKind;
+import com.powsybl.iidm.network.VoltageLevel;
+import com.powsybl.iidm.network.extensions.BusbarSectionPosition;
 import org.gridsuite.modification.ModificationType;
 import org.gridsuite.modification.dto.CreateVoltageLevelTopologyInfos;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Etienne Lesot <etienne.lesot at rte-france.com>
@@ -29,15 +35,32 @@ public class CreateVoltageLevelTopology extends AbstractModification {
     @Override
     public void apply(Network network, ReportNode subReportNode) {
         List<SwitchKind> switchKinds = new ArrayList<>();
+        VoltageLevel voltageLevel = network.getVoltageLevel(createVoltageLevelTopologyInfos.getVoltageLevelId());
         for (int i = 0; i < createVoltageLevelTopologyInfos.getSectionCount() - 1; i++) {
             switchKinds.add(SwitchKind.BREAKER);
         }
+        int lowBusOrBusbarIndex = findLowBusOrBusbarIndex(voltageLevel);
         new com.powsybl.iidm.modification.topology.CreateVoltageLevelTopologyBuilder()
             .withVoltageLevelId(createVoltageLevelTopologyInfos.getVoltageLevelId())
             .withSectionCount(createVoltageLevelTopologyInfos.getSectionCount())
             .withAlignedBusesOrBusbarCount(createVoltageLevelTopologyInfos.getAlignedBusesOrBusbarCount())
+            .withLowBusOrBusbarIndex(lowBusOrBusbarIndex)
             .withSwitchKinds(switchKinds)
             .build().apply(network, true, subReportNode);
+    }
+
+    private int findLowBusOrBusbarIndex(VoltageLevel voltageLevel) {
+        List<BusbarSection> busbarSections = voltageLevel.getNodeBreakerView()
+            .getBusbarSectionStream().toList();
+        List<BusbarSectionPosition> busbarExtensions = busbarSections.stream()
+            .map(busbarSection -> (BusbarSectionPosition) busbarSection.getExtension(BusbarSectionPosition.class))
+            .toList();
+        if (!busbarSections.isEmpty() && busbarExtensions.isEmpty()) {
+            throw new PowsyblException("busbar section position extension are not available, can not create a new busbar section");
+        }
+        Optional<BusbarSectionPosition> maxBusBarSectionPosition = busbarExtensions.stream()
+            .max(Comparator.comparingInt(BusbarSectionPosition::getBusbarIndex));
+        return maxBusBarSectionPosition.map(busbarSectionPosition ->  busbarSectionPosition.getBusbarIndex() + 1).orElse(1);
     }
 
     @Override
