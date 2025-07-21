686f1a064a24df4e81f716cdf10283ad08e1168c
Achour berrahma <berrahmaachour@gmail.com>
2025-07-21 10:35:18 +0200

    Add support for shunt compensator creation and update related messages (#77)
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

--- src/test/java/org/gridsuite/modification/modifications/tabularcreations/ShuntCompensatorCreationsTest.java
+++ src/test/java/org/gridsuite/modification/modifications/tabularcreations/ShuntCompensatorCreationsTest.java
@@ -140,4 +140,54 @@ class ShuntCompensatorCreationsTest extends AbstractNetworkModificationTest {
         assertEquals(ModificationType.SHUNT_COMPENSATOR_CREATION.name(), createdValues.get("tabularCreationType"));
     }
 
+    @Test
+    void testCheckCreationConflict() {
+        var shuntCreation = ShuntCompensatorCreationInfos
+                .builder()
+                .equipmentId("id1").equipmentName("name1").voltageLevelId("v1").busOrBusbarSectionId("1.1")
+                .connectionName("feederId1").connectionDirection(ConnectablePosition.Direction.TOP).connectionPosition(100).terminalConnected(true)
+                .sectionCount(1).maximumSectionCount(1)
+                .shuntCompensatorType(ShuntCompensatorType.REACTOR)
+                .maxQAtNominalV(1.0)
+                .maxSusceptance(10.0)
+                .build();
+
+        var shuntCreation2 = ShuntCompensatorCreationInfos
+                .builder()
+                .equipmentId("id2").equipmentName("name2").voltageLevelId("v1").busOrBusbarSectionId("1.1")
+                .connectionName("feederId2").connectionDirection(ConnectablePosition.Direction.TOP).connectionPosition(101).terminalConnected(true)
+                .sectionCount(1).maximumSectionCount(1)
+                .shuntCompensatorType(ShuntCompensatorType.REACTOR)
+                .maxSusceptance(20.0)
+                .build();
+
+        var shuntCreation3 = ShuntCompensatorCreationInfos
+                .builder()
+                .equipmentId("id3").equipmentName("name3").voltageLevelId("v1").busOrBusbarSectionId("1.1")
+                .connectionName("feederId3").connectionDirection(ConnectablePosition.Direction.TOP).connectionPosition(103).terminalConnected(true)
+                .sectionCount(1).maximumSectionCount(1)
+                .maxQAtNominalV(1.0)
+                .maxSusceptance(20.0)
+                .build();
+
+        var tabularCreationInfos = TabularCreationInfos
+                .builder()
+                .creationType(ModificationType.SHUNT_COMPENSATOR_CREATION)
+                .creations(List.of(shuntCreation, shuntCreation2, shuntCreation3))
+                .build();
+
+        ReportNode reportNode = tabularCreationInfos.createSubReportNode(ReportNode.newRootReportNode()
+                .withAllResourceBundlesFromClasspath()
+                .withMessageTemplate("test").build());
+        tabularCreationInfos.toModification().apply(getNetwork(), reportNode);
+
+        assertLogMessage("Tabular creation: Input for maximum susceptance has been ignored since it is not possible to simultaneously set type, maximum reactive power and maximum susceptance for shunt compensator with id id1",
+                "network.modification.tabular.creation.shuntCompensator.maxSusceptanceIgnored.1", reportNode);
+        assertLogMessage("Tabular creation: Input for maximum susceptance has been ignored since it is not possible to simultaneously set type and maximum susceptance for shunt compensator with id id2",
+                "network.modification.tabular.creation.shuntCompensator.maxSusceptanceIgnored.2", reportNode);
+        assertLogMessage("Tabular creation: Input for maximum susceptance has been ignored since it is not possible to simultaneously set maximum reactive power and maximum susceptance for shunt compensator with id id3",
+                "network.modification.tabular.creation.shuntCompensator.maxSusceptanceIgnored.3", reportNode);
+
+    }
+
 }
