7254ae402f5c97ca70ae1b5d9f9b7a573742e166
Joris Mancini <joris.mancini_externe@rte-france.com>
2025-07-23 13:41:54 +0200

    feat: add subtract load flow balancing option (#64)
    * Add support for "withLoadFlow" in balance adjustments
    * Enable load flow support in balance adjustments
    * feat: add subtract load flow balancing option
    * Upgrade to powsybl-balances-adjustment 2.14.1
    
    Signed-off-by: achour94 <berrahmaachour@gmail.com>
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>
    Co-authored-by: achour94 <berrahmaachour@gmail.com>
    Co-authored-by: Franck LECUYER <franck.lecuyer@rte-france.com>
    Co-authored-by: FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>

--- src/main/java/org/gridsuite/modification/modifications/BalancesAdjustmentModification.java
+++ src/main/java/org/gridsuite/modification/modifications/BalancesAdjustmentModification.java
@@ -6,10 +6,7 @@
  */
 package org.gridsuite.modification.modifications;
 
-import com.powsybl.balances_adjustment.balance_computation.BalanceComputation;
-import com.powsybl.balances_adjustment.balance_computation.BalanceComputationArea;
-import com.powsybl.balances_adjustment.balance_computation.BalanceComputationFactoryImpl;
-import com.powsybl.balances_adjustment.balance_computation.BalanceComputationParameters;
+import com.powsybl.balances_adjustment.balance_computation.*;
 import com.powsybl.balances_adjustment.util.CountryAreaFactory;
 import com.powsybl.balances_adjustment.util.Reports;
 import com.powsybl.commons.report.ReportNode;
@@ -120,6 +117,7 @@ public class BalancesAdjustmentModification extends AbstractModification {
         parameters.setMaxNumberIterations(balancesAdjustmentModificationInfos.getMaxNumberIterations());
         parameters.setThresholdNetPosition(balancesAdjustmentModificationInfos.getThresholdNetPosition());
         parameters.setMismatchMode(BalanceComputationParameters.MismatchMode.MAX);
+        parameters.setSubtractLoadFlowBalancing(balancesAdjustmentModificationInfos.isSubtractLoadFlowBalancing());
         parameters.getLoadFlowParameters().setCountriesToBalance(
                 new HashSet<>(balancesAdjustmentModificationInfos.getCountriesToBalance())
         );
