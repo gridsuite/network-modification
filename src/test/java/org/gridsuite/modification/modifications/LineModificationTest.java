9a8f9dd294521457e1ba7bd06304f77facdc01c9
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-10-16 13:48:53 +0200

    checkstyle
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/test/java/org/gridsuite/modification/modifications/LineModificationTest.java
+++ src/test/java/org/gridsuite/modification/modifications/LineModificationTest.java
@@ -21,8 +21,6 @@ import org.gridsuite.modification.dto.*;
 import org.gridsuite.modification.utils.NetworkCreation;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
