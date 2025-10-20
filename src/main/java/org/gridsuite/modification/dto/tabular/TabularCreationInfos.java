0716349e565289f307b98ace0c937b37b8d78dc2
dbraquart <107846716+dbraquart@users.noreply.github.com>
2025-10-20 15:00:56 +0200

    Use a common dto and apply for tabular modifications (#101)
    Signed-off-by: David BRAQUART <david.braquart@rte-france.com>

--- src/main/java/org/gridsuite/modification/dto/tabular/TabularCreationInfos.java
+++ src/main/java/org/gridsuite/modification/dto/tabular/TabularCreationInfos.java
@@ -4,9 +4,8 @@
   License, v. 2.0. If a copy of the MPL was not distributed with this
   file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package org.gridsuite.modification.dto;
+package org.gridsuite.modification.dto.tabular;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.powsybl.commons.report.ReportNode;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,14 +13,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.gridsuite.modification.ModificationType;
 import org.gridsuite.modification.dto.annotation.ModificationErrorTypeName;
 import org.gridsuite.modification.modifications.AbstractModification;
-import org.gridsuite.modification.modifications.TabularCreation;
-import org.springframework.lang.NonNull;
+import org.gridsuite.modification.modifications.tabular.TabularCreation;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -36,27 +32,9 @@ import java.util.Map;
 @ModificationErrorTypeName("TABULAR_CREATION_ERROR")
 public class TabularCreationInfos extends TabularBaseInfos {
 
-    @Schema(description = "Creation type")
-    @NonNull
-    private ModificationType creationType;
-
-    @Schema(description = "Creations")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private List<ModificationInfos> creations;
-
     @Override
     public AbstractModification toModification() {
         return new TabularCreation(this);
-    }
-
-    public String formatEquipmentTypeName() {
-        return switch (getCreationType()) {
-            case GENERATOR_CREATION -> getCreations().size() > 1 ? "generators" : "generator";
-            case LOAD_CREATION -> getCreations().size() > 1 ? "loads" : "load";
-            case SHUNT_COMPENSATOR_CREATION -> getCreations().size() > 1 ? "shunt compensators" : "shunt compensator";
-            case BATTERY_CREATION -> getCreations().size() > 1 ? "batteries" : "battery";
-            default -> "equipments of unknown type";
-        };
     }
 
     @Override
@@ -70,7 +48,7 @@ public class TabularCreationInfos extends TabularBaseInfos {
     @Override
     public Map<String, String> getMapMessageValues() {
         Map<String, String> mapMessageValues = new HashMap<>();
-        mapMessageValues.put("tabularCreationType", getCreationType().name());
+        mapMessageValues.put("tabularCreationType", getModificationType().name());
         return mapMessageValues;
     }
 }
