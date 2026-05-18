f91109b99c24b1ac075d2ec50c3efd7d95f25b4a
Rehili Ghazwa <ghazwarhili@gmail.com>
2026-05-18 23:43:55 +0200

    draft vr

--- src/test/java/org/gridsuite/modification/modifications/byfilter/formula/AbstractByFormulaModificationTest.java
+++ src/test/java/org/gridsuite/modification/modifications/byfilter/formula/AbstractByFormulaModificationTest.java
@@ -46,14 +46,14 @@ abstract class AbstractByFormulaModificationTest extends AbstractNetworkModifica
     protected static final UUID FILTER_ID_7 = UUID.randomUUID();
     protected static final UUID FILTER_WITH_ALL_WRONG_IDS = UUID.randomUUID();
     protected static final UUID FILTER_WITH_ONE_WRONG_ID = UUID.randomUUID();
-    protected final FilterInfos filter1 = new FilterInfos(FILTER_ID_1, "filter1");
-    protected final FilterInfos filter2 = new FilterInfos(FILTER_ID_2, "filter2");
-    protected final FilterInfos filter3 = new FilterInfos(FILTER_ID_3, "filter3");
-    protected final FilterInfos filter4 = new FilterInfos(FILTER_ID_4, "filter4");
-    protected final FilterInfos filter5 = new FilterInfos(FILTER_ID_5, "filter5");
-    protected final FilterInfos filter6 = new FilterInfos(FILTER_ID_6, "filter6");
-    protected final FilterInfos filter7 = new FilterInfos(FILTER_ID_7, "filter7");
-    protected final FilterInfos filterWithOneWrongId = new FilterInfos(FILTER_WITH_ONE_WRONG_ID, "filterWithOneWrongId");
+    protected final FilterInfos filter1 = new FilterInfos(FILTER_ID_1, "filter1", null);
+    protected final FilterInfos filter2 = new FilterInfos(FILTER_ID_2, "filter2", null);
+    protected final FilterInfos filter3 = new FilterInfos(FILTER_ID_3, "filter3", null);
+    protected final FilterInfos filter4 = new FilterInfos(FILTER_ID_4, "filter4", null);
+    protected final FilterInfos filter5 = new FilterInfos(FILTER_ID_5, "filter5", null);
+    protected final FilterInfos filter6 = new FilterInfos(FILTER_ID_6, "filter6", null);
+    protected final FilterInfos filter7 = new FilterInfos(FILTER_ID_7, "filter7", null);
+    protected final FilterInfos filterWithOneWrongId = new FilterInfos(FILTER_WITH_ONE_WRONG_ID, "filterWithOneWrongId", null);
     protected final ReportNode reportNode = ReportNode.newRootReportNode()
             .withResourceBundles(NetworkModificationReportResourceBundle.BASE_NAME)
             .withMessageTemplate("test")
