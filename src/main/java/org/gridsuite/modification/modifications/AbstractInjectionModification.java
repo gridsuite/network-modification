0b1a10c550644b707327b871cf98f37c2dcd01cd
Kamil MARUT <kamil.marut@rte-france.com>
2026-06-16 11:07:40 +0200

    feat(hvdc-vsc-converter-station-measurements): handle p and q measurement modification in HVDC VSC converter station

--- src/main/java/org/gridsuite/modification/modifications/AbstractInjectionModification.java
+++ src/main/java/org/gridsuite/modification/modifications/AbstractInjectionModification.java
@@ -8,15 +8,8 @@ package org.gridsuite.modification.modifications;
 
 import com.powsybl.commons.report.ReportNode;
 import com.powsybl.iidm.network.Injection;
-import com.powsybl.iidm.network.extensions.Measurement;
-import com.powsybl.iidm.network.extensions.Measurements;
-import com.powsybl.iidm.network.extensions.MeasurementsAdder;
 import org.gridsuite.modification.dto.InjectionModificationInfos;
 import org.gridsuite.modification.utils.MeasurementUtils;
-import org.gridsuite.modification.utils.ModificationUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Ayoub LABIDI <ayoub.labidi at rte-france.com>
@@ -29,32 +22,7 @@ public abstract class AbstractInjectionModification extends AbstractModification
         this.modificationInfos = modificationInfos;
     }
 
-    protected ReportNode updateMeasurements(Injection<?> injection, InjectionModificationInfos injectionModificationInfos, ReportNode subReportNode) {
-        Double pValue = injectionModificationInfos.getPMeasurementValue() != null ? injectionModificationInfos.getPMeasurementValue().getValue() : null;
-        Double qValue = injectionModificationInfos.getQMeasurementValue() != null ? injectionModificationInfos.getQMeasurementValue().getValue() : null;
-        Boolean pValidity = injectionModificationInfos.getPMeasurementValidity() != null ? injectionModificationInfos.getPMeasurementValidity().getValue() : null;
-        Boolean qValidity = injectionModificationInfos.getQMeasurementValidity() != null ? injectionModificationInfos.getQMeasurementValidity().getValue() : null;
-
-        if (pValue == null && pValidity == null && qValue == null && qValidity == null) {
-            // no measurement modification requested
-            return null;
-        }
-        Measurements<?> measurements = (Measurements<?>) injection.getExtension(Measurements.class);
-        if (measurements == null) {
-            MeasurementsAdder<?> measurementsAdder = injection.newExtension(MeasurementsAdder.class);
-            measurements = measurementsAdder.add();
-        }
-        // measurements update
-        List<ReportNode> reports = new ArrayList<>();
-        MeasurementUtils.upsertMeasurement(measurements, Measurement.Type.ACTIVE_POWER, pValue, pValidity, reports);
-        MeasurementUtils.upsertMeasurement(measurements, Measurement.Type.REACTIVE_POWER, qValue, qValidity, reports);
-        // report changes
-        ReportNode estimSubReportNode = null;
-        if (!reports.isEmpty()) {
-            estimSubReportNode = subReportNode.newReportNode().withMessageTemplate("network.modification.stateEstimationData").add();
-            ModificationUtils.getInstance().reportModifications(estimSubReportNode, reports, "network.modification.measurements");
-        }
-        return estimSubReportNode;
+    protected ReportNode updateMeasurements(Injection<?> injection, ReportNode subReportNode) {
+        return MeasurementUtils.applyAndBuildModificationReport(injection, modificationInfos, subReportNode);
     }
-
 }
