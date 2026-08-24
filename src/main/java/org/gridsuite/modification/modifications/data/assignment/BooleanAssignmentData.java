fa42cc29823c0fab26af452ec88a21ac04d8d0cb
Joris Mancini <joris.mancini_externe@rte-france.com>
2026-08-24 10:36:38 +0200

    create data structures
    Signed-off-by: Joris Mancini <joris.mancini_externe@rte-france.com>

---
+++ src/main/java/org/gridsuite/modification/modifications/data/assignment/BooleanAssignmentData.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.gridsuite.modification.modifications.data.assignment;
+
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.gridsuite.modification.dto.byfilter.assignment.DataType;
+
+/**
+ * @author Joris MANCINI <joris.mancini at rte-france.com>
+ */
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class BooleanAssignmentData extends ValueAssignmentData<Boolean> {
+
+    @Override
+    public DataType getDataType() {
+        return DataType.BOOLEAN;
+    }
+}
