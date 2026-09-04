27057d0dd8a4c5495e1abb76ec64c63de1c822f6
achour94 <berrahmaachour@gmail.com>
2026-09-04 14:18:02 +0200

    Refactor and extend modification context with new loaders and dependency handling
    Signed-off-by: achour94 <berrahmaachour@gmail.com>

--- src/main/java/org/gridsuite/modification/context/FilterLoader.java
+++ src/main/java/org/gridsuite/modification/context/FilterLoader.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package org.gridsuite.modification;
+package org.gridsuite.modification.context;
 
 import org.gridsuite.filter.wip.Filter;
 
@@ -15,12 +15,19 @@ import java.util.UUID;
 /**
  * Resolves filter definitions from their identifiers.
  *
+ * <p>A resolved {@link Filter} is self-contained: it carries everything needed to evaluate it against a
+ * network.
+ *
  * @author Achour BERRAHMA <achour.berrahma at rte-france.com>
  */
+@FunctionalInterface
 public interface FilterLoader {
 
     /**
      * Loads the filters matching the given identifiers.
+     *
+     * <p>Filters that cannot be found are <b>silently omitted</b>: they are simply absent from the returned
+     * map, whose values are never {@code null}.
      *
      * @param filterUuids the identifiers of the filters to load
      * @return the filters found, indexed by their identifier
